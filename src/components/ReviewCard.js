45750ce1fa6c5cc9194e3eace11ad06c00cdb57a
Tacobeanzz <thisistacotaco@gmail.com>
2021-09-16 21:55:50 +0100

    reviewcard

---
+++ src/components/ReviewCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ReviewCard.css'
+
+const ReviewCard = () => {
+    return (
+        <div>
+            <div className="card">
+                <div className="avatar">
+
+                </div>
+                <div className="name"><h1>Name SecondName</h1></div>
+                <div className="reviewtext"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, excepturi!</p></div>
+            </div>
+        </div>
+    )
+}
+
+export default ReviewCard
